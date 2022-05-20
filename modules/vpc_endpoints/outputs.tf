7a14279d927616b9382a38017da63b3ffffc15e6
Luigi Di Fraia <93160889+luigidifraiawork@users.noreply.github.com>
2022-05-20 19:08:40 +0100

    Update outputs.tf

--- modules/vpc_endpoints/outputs.tf
+++ modules/vpc_endpoints/outputs.tf
@@ -1,2 +1,7 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: MIT-0 */
+
+output "route_table_endpoint_id" {
+  value       = aws_route_table.endpoint_vpc.id
+  description = "Route table ID for endpoint VPC"
+}
