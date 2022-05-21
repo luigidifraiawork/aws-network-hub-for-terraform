69156f718c9c8c69e1d6d9e438ec5ab73acfe704
Luigi Di Fraia <93160889+luigidifraiawork@users.noreply.github.com>
2022-05-21 09:20:40 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
    SPDX-License-Identifier: MIT-0 */
 
-# output "tgw_route_table" {
-#   description = "map of route tables used for association and propagation"
-#   value       = module.tgw.tgw_route_table
-# }
+output "tgw_route_table" {
+  description = "map of route tables used for association and propagation"
+  value       = module.tgw.tgw_route_table
+}
 
 # output "firewall_info" {
 #   value       = module.network_firewall_vpc.firewall_info
